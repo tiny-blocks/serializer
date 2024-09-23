a4fced1b2a9cfca78bc1575264c417056a35872b
Gustavo Freze <gustavo.freze@outlook.com>
2024-09-23 10:02:11 -0300

    Release/3.2.1 (#15)

--- src/SerializerAdapter.php
+++ src/SerializerAdapter.php
@@ -7,11 +7,17 @@ namespace TinyBlocks\Serializer;
 use TinyBlocks\Serializer\Internal\ArraySerializer;
 use TinyBlocks\Serializer\Internal\JsonSerializer;
 
+/**
+ * A trait that provides serialization methods to convert objects into JSON and array formats.
+ *
+ * @template Key of array-key
+ * @template Value of mixed
+ */
 trait SerializerAdapter
 {
-    public function toJson(): string
+    public function toJson(SerializeKeys $serializeKeys = SerializeKeys::PRESERVE): string
     {
-        return (new JsonSerializer())->serialize(data: $this->toArray());
+        return (new JsonSerializer())->serialize(data: $this->toArray(serializeKeys: $serializeKeys));
     }
 
     public function toArray(SerializeKeys $serializeKeys = SerializeKeys::PRESERVE): array
